fd9819c97219d1e0523d21dde53f15193eedde0b
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2021-01-30 14:37:33 +0000

    Adding the original license terms from Microsoft, as shown on @apexskier's own files

--- Scripts/lsp.js
+++ Scripts/lsp.js
@@ -1,5 +1,24 @@
 // Turn a Nova start-end range to an LSP row-column range.
 // From https://github.com/apexskier/nova-typescript
+//
+// Adding the original license terms from Microsoft, as shown on @apexskier's
+//  own files (gwyneth 20200130)
+//
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */
+//
 exports.RangeToLspRange = function (document, range) {
   const fullContents = document.getTextInRange(new Range(0, document.length));
   let chars = 0;
