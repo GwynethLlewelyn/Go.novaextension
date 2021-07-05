52dee5ccbea61d8356eda5ce3971896ed533ae8c
Gwyneth Llewelyn <gwyneth.llewelyn@gwynethllewelyn.net>
2021-07-06 00:56:25 +0100

    Updating README

--- Tests/basictest.go
+++ Tests/basictest.go
@@ -9,7 +9,8 @@ import (
 )
 
 // foo bar cat.
-func foo(s string) {
+func foo(s string) string {
+	return s
 }
 
 type User struct {
@@ -24,8 +25,13 @@ func myFunc(a string, b string) bool {
 	return a != b // no? why the weird error?
 }
 
+// anotherFunc serves no purpose whatsoever.
+func anotherFunc(a, b string) string {
+	return a + b
+}
+
 // Greeting is a list of methods.
-func (u *User) Greeting(what string) string {
+func (u *User) Greeting(what string, users User) string {
 	var blip myPtr.Atom
 	return fmt.Sprintf("Dear %s %s %v: %s", u.FirstName, u.LastName, blip, what)
 }
@@ -43,12 +49,14 @@ func main() {
 
 	// var a = 'w'
 	var a []byte
+	var v = 1
 	t := make([]byte, 1, 2)
 	b := append(t, a[0], byte(1), 0.00, byte(0x08))
 	complexNumber := -4.0436 + 3.76e-5i // almost correctly formatted/captured by the regex
 	exponential := -3.167252381e-12
 	hex := 0x0f - 0x0f0f
 	octal := 0777
+	v = octal + hex
 
 	var (
 		google []byte
@@ -58,7 +66,7 @@ func main() {
 	Main := "var"
 	heredoc := `hi`
 	fmt.Println("nothing works...", heredoc) // was
-	fmt.Println("Clearly nothing yet...", google, x, c, t, b, complexNumber, exponential, octal)
+	fmt.Println("Clearly nothing yet...", google, x, c, t, b, v, complexNumber, exponential, octal)
 	fmt.Println("wth?...")
 	println("something")
 	fmt.Printf("%s%v\n", Main, hex)
@@ -73,7 +81,7 @@ func main() {
 
 	if i > 0 && err != nil {
 		// do something
-		pointer.Greeting(os.TempDir())
+		pointer.Greeting(os.TempDir(), *pointer)
 	} else {
 		// do something else somewhere
 		fmt.Println("Something else")
